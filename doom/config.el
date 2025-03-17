e187fba2ea1c96d2df51b8b4a9c0114647303477
scrwww <nulltillasn@gmcil.com>
2025-03-17 20:20:46 -0300

    some chages again

--- doom/config.el
+++ doom/config.el
@@ -34,6 +34,7 @@
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-gruvbox)
 
+
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
@@ -41,6 +42,13 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/personal/org")
+(setq org-archive-location "~/personal/org/archive/%s_archive::")
+
+(setq
+ projectile-project-search-path '("~/dev/")
+ display-line-numbers-type 'relative
+)
+
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
