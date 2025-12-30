487d69105523b986b2ccba0da736b1c2be606c09
scrwww <nulltillasn@gmail.com>
2025-12-30 14:44:10 -0300

    foot kanso

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,2 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
-
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
+function fish_greeting
+end
