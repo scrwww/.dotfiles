4b3e3a5e1d57e0bc23128fdf662366e91d60f98a
scrwww <nulltillasn@gmail.com>
2026-01-10 13:49:27 -0300

    del: .config dir

--- .config/rofi/cliphistory.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-theme="kanso-rofi-clip"
-dir="$HOME/.config/rofi"
-
-rofi -modi clipboard:$dir/"cliphist-rofi-img.sh" -show clipboard -show-icons -theme $dir/"$theme"
--- .config/rofi/launcher.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-theme="kanso-rofi"
-dir="$HOME/.config/rofi"
-
-rofi -no-lazy-grab -show drun -modi drun -no-fixed-num-lines -lines 5 -theme $dir/"$theme"
