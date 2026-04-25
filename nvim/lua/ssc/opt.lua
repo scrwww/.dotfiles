61ebc3e23742f27013515c901787625d0745ccb6
scrwww <119636547+scrwww@users.noreply.github.com>
2026-04-25 00:16:44 -0300

    cmp changes for nvim

--- nvim/lua/ssc/opt.lua
+++ nvim/lua/ssc/opt.lua
@@ -3,9 +3,9 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 8
+vim.opt.shiftwidth = 8
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -27,4 +27,4 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "148"
--- nvim/lua/ssc/plugins/lsp.lua
+++ nvim/lua/ssc/plugins/lsp.lua
@@ -1,107 +1,87 @@
-return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
+return{
         "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
-    },
+        dependencies  = {
+                { "folke/lazydev.nvim",               opts = {} },
+                { "neovim/nvim-lspconfig" },
+                { "williamboman/mason-lspconfig.nvim" },
+                { "j-hui/fidget.nvim",     opts = {}, },
+                { "folke/tokyonight.nvim", priority = 1000, opts = {} },
+        },
 
-    config = function()
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
+        config = function()
+                require("mason").setup()
+                require("mason-lspconfig").setup()
+                require("mason-lspconfig").setup_handlers({
+                        function(server_name)
+                                require("lspconfig")[server_name].setup({})
+                        end,
+                })
+                vim.cmd([[autocmd FileType * set formatoptions-=ro]]) -- disable new line auto comment
+                ---]]
 
-        require("fidget").setup({})
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "rust_analyzer",
-                "ts_ls",
-            },
-            handlers = {
-                function(server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
 
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
-                        },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
+                ---[[AUTOCOMPLETION SETUP
+                vim.o.completeopt = "menu,noinsert,popup,fuzzy"
 
-                end,
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    }
-                end,
-            }
-        })
+                ---[[ Setup keymaps so we can accept completion using Enter and choose items using arrow keys or Tab.
+                local pumMaps = {
+                        ['<Tab>'] = '<C-n>',
+                        ['<S-Tab>'] = '<C-p>',
+                        ['<Down>'] = '<C-n>',
+                        ['<Up>'] = '<C-p>',
+                        ['<CR>'] = '<C-y>',
+                }
+                for insertKmap, pumKmap in pairs(pumMaps) do
+                        vim.keymap.set('i', insertKmap, function()
+                                return vim.fn.pumvisible() == 1 and pumKmap or insertKmap
+                        end, { expr = true })
+                end
+                ---]]
 
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+                vim.api.nvim_create_autocmd('LspAttach', {
+                        callback = function(args)
+                                ---[[Code required to activate autocompletion and trigger it on each keypress
+                                local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+                                client.server_capabilities.completionProvider.triggerCharacters = vim.split("qwertyuiopasdfghjklzxcvbnm. ", "")
+                                vim.api.nvim_create_autocmd({ 'TextChangedI' }, {
+                                        buffer = args.buf,
+                                        callback = function()
+                                                vim.lsp.completion.get()
+                                        end
+                                })
+                                vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+                                ---]]
 
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-            }, {
-                { name = 'buffer' },
-            })
-        })
-
-        vim.diagnostic.config({
-            -- update_in_insert = true,
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
-        })
-    end
+                                ---[[Code required to add documentation popup for an item
+                                local _, cancel_prev = nil, function() end
+                                vim.api.nvim_create_autocmd('CompleteChanged', {
+                                        buffer = args.buf,
+                                        callback = function()
+                                                cancel_prev()
+                                                local info = vim.fn.complete_info({ 'selected' })
+                                                local completionItem = vim.tbl_get(vim.v.completed_item, 'user_data', 'nvim', 'lsp', 'completion_item')
+                                                if nil == completionItem then
+                                                        return
+                                                end
+                                                _, cancel_prev = vim.lsp.buf_request(args.buf,
+                                                vim.lsp.protocol.Methods.completionItem_resolve,
+                                                completionItem,
+                                                function(err, item, ctx)
+                                                        if not item then
+                                                                return
+                                                        end
+                                                        local docs = (item.documentation or {}).value
+                                                        local win = vim.api.nvim__complete_set(info['selected'], { info = docs })
+                                                        if win.winid and vim.api.nvim_win_is_valid(win.winid) then
+                                                                vim.treesitter.start(win.bufnr, 'markdown')
+                                                                vim.wo[win.winid].conceallevel = 3
+                                                        end
+                                                end)
+                                        end
+                                })
+                                ---]]
+                        end
+                })
+                ---AUTOCOMPLETION SETUP END]]
+        end
 }
