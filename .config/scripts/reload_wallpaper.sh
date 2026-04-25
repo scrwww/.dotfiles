32193c40a6d8367aa8b90addf655f7f3a7d2dd98
scrwww <119636547+scrwww@users.noreply.github.com>
2026-04-25 00:16:45 -0300

    del: .config dir

--- .config/scripts/reload_wallpaper.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-last_wallpaper="$HOME/.cache/last-wallpaper"
-[[ -f "$last_wallpaper" ]] || exit 0
-wallpaper=$(<"$last_wallpaper")
-
-monitor=$(hyprctl monitors | grep Monitor | awk '{print $2}')
-hyprctl hyprpaper preload "$wallpaper"
-hyprctl hyprpaper wallpaper "$monitor,$wallpaper"
