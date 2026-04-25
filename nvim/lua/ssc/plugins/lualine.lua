823806a06dc0893cb5611b64b48b537ec12b1b08
scrwww <119636547+scrwww@users.noreply.github.com>
2026-04-25 00:16:44 -0300

    adding files nvim tmux scripts

--- nvim/lua/ssc/plugins/lualine.lua
+++ nvim/lua/ssc/plugins/lualine.lua
@@ -7,8 +7,8 @@ return {
       options = {
         icons_enabled = true,
         theme = 'auto',
-        component_separators = { left = '', right = ''},
-        section_separators = { left = '', right = ''},
+        component_separators = { left = '|', right = '|'},
+        section_separators = { left = '|', right = '|'},
         disabled_filetypes = {
           statusline = {},
           winbar = {},
@@ -17,17 +17,17 @@ return {
         always_divide_middle = true,
         globalstatus = false,
         refresh = {
-          statusline = 1000,
-          tabline = 1000,
-          winbar = 1000,
+          statusline = 10,
+          tabline = 500,
+          winbar = 500,
         }
       },
       sections = {
-        lualine_a = {'mode'},
+        lualine_a = {'buffers'},
         lualine_b = {'branch', 'diff', 'diagnostics'},
         lualine_c = {'filename'},
         lualine_x = {'encoding', 'fileformat', 'filetype'},
-        lualine_y = {'progress'},
+        lualine_y = {},
         lualine_z = {'location'}
       },
       inactive_sections = {
