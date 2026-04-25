efbc4b969d78cc31c79f187f8fa186478e47590f
scrwww <119636547+scrwww@users.noreply.github.com>
2026-04-25 00:16:44 -0300

    added zsh config and added obsidian.lua plugin

--- .config/nvim/lua/ssc/plugins/orgmode.lua
+++ .config/nvim/lua/ssc/plugins/orgmode.lua
@@ -7,12 +7,36 @@ return{
             load = {
                 ["core.defaults"] = {},
                 ["core.concealer"] = {},
+                ["core.ui.calendar"] = {},
+                ["core.summary"] = {},
                 ["core.dirman"] = {
                     config = {
                         workspaces = {
                             notes = "~/personal/org/notes",
+                            school = "~/personal/org/school",
                         },
                         default_workspace = "notes",
+                    },
+                },
+                ["core.keybinds"] = {
+                    config = {
+                        default_keybinds = true,
+                        vim.api.nvim_create_autocmd("Filetype", {
+                            pattern = "norg",
+                            callback = function()
+                                vim.keymap.set("n", "<LocalLeader>ip", "<Plug>(neorg.tempus.insert-date.insert-mode)", { buffer = true })
+                            end,
+                        })
+                    },
+                },
+                ["core.export"] = {
+                    config = {
+                        export_dir = "~/personal/org/exports",
+                    },
+                },
+                ["core.completion"] = {
+                    config = {
+                        engine = "nvim-cmp",
                     },
                 },
             },
